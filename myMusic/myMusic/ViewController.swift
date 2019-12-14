1d467d27691057bc60577f6f201223a4e2a7f6fb
Tim Geldof <Tim.geldof@student.hogent.be>
2019-12-14 16:44:48 +0100

    Init project

---
+++ myMusic/myMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myMusic
+//
+//  Created by Tim Geldof on 14/12/2019.
+//  Copyright © 2019 Tim Geldof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ myMusic/myMusicTests/myMusicTests.swift
@@ -0,0 +1,34 @@
+//
+//  myMusicTests.swift
+//  myMusicTests
+//
+//  Created by Tim Geldof on 14/12/2019.
+//  Copyright © 2019 Tim Geldof. All rights reserved.
+//
+
+import XCTest
+@testable import myMusic
+
+class myMusicTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
