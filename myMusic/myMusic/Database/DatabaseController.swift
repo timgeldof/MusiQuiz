df8f01d47f1a40534c076612c70d7eb74e088db0
Tim Geldof <Tim.geldof@student.hogent.be>
2019-12-15 17:31:45 +0100

    Add database controller

---
+++ myMusic/myMusic/Database/DatabaseController.swift
@@ -0,0 +1,17 @@
+//
+//  DatabaseController.swift
+//  myMusic
+//
+//  Created by Tim Geldof on 15/12/2019.
+//  Copyright © 2019 Tim Geldof. All rights reserved.
+//
+
+import Foundation
+
+
+
+class DatabaseController {
+    static let sharedInstance = DatabaseController()
+
+    
+}
--- myMusic/myMusic/ViewControllers/FavoriteTracksViewController.swift
+++ myMusic/myMusic/ViewControllers/FavoriteTracksViewController.swift
@@ -8,12 +8,34 @@
 
 import UIKit
 
-class FavoriteTracksViewController: UIViewController {
+class FavoriteTracksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var tracks : [SearchTrackResponse]  = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tracks.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) ->Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TrackTableViewCell
+        cell.backgroundColor = UIColor.lightGray
+        cell.update(with: self.tracks[indexPath.row])
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        performSegue(withIdentifier: "showDetailSearchedTrack", sender: self)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 
